3a532e3abe36eaa3eef7efbf2caffc40f3efae26
Vyacheslav Fedorov <fuodorov1998@gmail.com>
2022-02-21 15:30:45 +0700

    Update polynomial_hash.py

--- sprint_4/polynomial_hash.py
+++ sprint_4/polynomial_hash.py
@@ -2,5 +2,15 @@ base = int(input())
 tablesize = int(input())
 string = input()
 
-lenght = len(string)
-print(sum([base ** (lenght - pos - 1) * ord(char) for pos, char in enumerate(string)]) % tablesize)
+
+def polynomial_hash(str, p, m):
+    power_of_p = 1
+    hash_val = 0
+
+    for char in str:
+        hash_val = ((hash_val + ord(char) * power_of_p) % m)
+        power_of_p = (power_of_p * p) % m
+ 
+    return int(hash_val)
+
+print(polynomial_hash(string[::-1], base, tablesize))
