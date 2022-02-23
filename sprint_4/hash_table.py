998db1231361f003a0a591bc1f40fd84cc04492b
Vyacheslav Fedorov <fuodorov1998@gmail.com>
2022-02-23 18:16:36 +0700

    update hash table

--- sprint_4/hash_table.py
+++ sprint_4/hash_table.py
@@ -1,69 +1,101 @@
 """
 ПРИНЦИП РАБОТЫ
+    Абстрактный тип данных Map можно определить следующим образом.
+    Его структура - неупорядоченная коллекция ассоциаций между ключами и значениями.
+    Все ключи уникальны, таким образом поддерживаются отношения “один к одному” между ключами и значениями.
+
+    Мы используем два списка, чтобы создать класс HashTable, воплощающий абстрактный тип данных Map.
+    Один список, называемый slots, будет содержать ключи элементов,
+    а параллельный ему список data - значения данных.
+    Когда мы находим ключ, на соответствующей позиции в списке с данными будет находиться связанное с ним значение.
+
+    hash функция реализует простой метод остатков.
+    В качестве техники разрешения коллизий используется линейное пробирование с функцией повторного хэширования.
+
+    Функция put предполагает, что в конце-концов найдётся пустой слот, или такой ключ уже присутствует в self.slots.
+    Она вычисляет оригинальное хэш-значение и, если слот не пуст, применяет функцию rehash до тех пор,
+    пока не найдёт свободное место. Если непустой слот уже содержит ключ, старое значение будет заменено на новое.
+
+    Аналогично функция get начинает с вычисления начального хэш-значения.
+    Если искомая величина не содержится в этом слоте, то используется rehash для определения следующей позиции.
+
+    Вдохновлен - http://aliev.me/runestone/SortSearch/Hashing.html
 
 ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ
+    Проблема коллизий. Когда два элемента хэшируются в один слот,
+    нам требуется систематический метод для размещения в хэш-таблице второго элемента.
+    Одним из методов разрешения коллизий является просмотр хэш-таблицы и
+    поиск другого свободного слота для размещения в нём элемента, создавшего проблему.
+    Простой способ сделать это - начать с оригинальной позиции хэш-значения и
+    перемещаться по слотам определённым образом до тех пор, пока не будет найден пустой.
+    Нам может понадобиться вернуться обратно к первому слоту (циклически), чтобы охватить хэш-таблицу целиком.
+    Этот процесс разрешения коллизий называется открытой адресацией,
+    поскольку пытается найти следующий свободный слот (или адрес) в хэш-таблице.
+    Систематически посещая каждый слот по одному разу,
+    мы действуем в соответствии с техникой открытой адресации, называемой линейным пробированием.
 
 ВРЕМЕННАЯ СЛОЖНОСТЬ
+    Когда мы хотим найти элемент, мы просто используем хэш-функцию,
+    чтобы вычислить имя слота элемента и затем проверить по таблице его наличие.
+    Эта операция поиска имеет O(1), поскольку на вычисление хэш-значения требуется константное время,
+    как и на переход по найденному индексу.
+    Если всё находится там, где ему положено, то мы получаем алгоритм поиска за константное время.
 
 ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ
+    Map, максимальный размер которого определяется заданным числом k, занимает O(k) памяти.
 
 """
 
 
 class HashTable:
     def __init__(self, size=10**5):
-        self.size = size
-        self.slots = [None] * self.size
-        self.data = [None] * self.size
+        self._size = size
+        self._slots = [None] * self._size
+        self._data = [None] * self._size
 
     def put(self, key, data):
-        hashvalue = self.hashfunction(key, len(self.slots))
+        slot = self.hash(key)
 
-        if self.slots[hashvalue] is None:
-            self.slots[hashvalue] = key
-            self.data[hashvalue] = data
+        if self._slots[slot] is None:
+            self._slots[slot] = key
+            self._data[slot] = data
+        elif self._slots[slot] == key:
+            self._data[slot] = data
         else:
-            if self.slots[hashvalue] == key:
-                self.data[hashvalue] = data
+            next_slot = self.rehash(slot)
+            while self._slots[next_slot] is not None and self._slots[next_slot] != key:
+                next_slot = self.rehash(next_slot)
+
+            if self._slots[next_slot] is None:
+                self._slots[next_slot] = key
+                self._data[next_slot] = data
             else:
-                nextslot = self.rehash(hashvalue, len(self.slots))
-                while self.slots[nextslot] is not None and self.slots[nextslot] != key:
-                    nextslot = self.rehash(nextslot, len(self.slots))
+                self._data[next_slot] = data
 
-                if self.slots[nextslot] is None:
-                    self.slots[nextslot] = key
-                    self.data[nextslot] = data
-                else:
-                    self.data[nextslot] = data
+    def hash(self, key):
+        return key % self._size
 
-    @staticmethod
-    def hashfunction(key, size):
-        return key % size
-
-    @staticmethod
-    def rehash(oldhash, size):
-        return (oldhash + 1) % size
+    def rehash(self, old_hash):
+        return (old_hash + 1) % self._size
 
     def get(self, key):
-        startslot = self.hashfunction(key, len(self.slots))
-
         data = None
-        stop = False
-        found = False
-        position = startslot
-        while self.slots[position] is not None and not found and not stop:
-            if self.slots[position] == key:
+        stop, found = False, False
+        position = slot = self.hash(key)
+        while self._slots[position] is not None and not found and not stop:
+            if self._slots[position] == key:
                 found = True
-                data = self.data[position]
+                data = self._data[position]
             else:
-                position = self.rehash(position, len(self.slots))
-                if position == startslot:
+                position = self.rehash(position)
+                if position == slot:
                     stop = True
         return data
 
     def delete(self, key):
         data = self.get(key)
-        self.put(key, None)
+        if data is not None:
+            self.put(key, None)
         return data
 
     def __getitem__(self, key):
