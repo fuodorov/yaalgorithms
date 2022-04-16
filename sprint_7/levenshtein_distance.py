255985d5f21d77a2962e6c1bf56ab43ba3f3d800
Vyacheslav Fedorov <fuodorov1998@gmail.com>
2022-04-16 09:31:37 +0700

    Update levenshtein_distance.py

--- sprint_7/levenshtein_distance.py
+++ sprint_7/levenshtein_distance.py
@@ -1,15 +1,37 @@
-def levenshtein_distance(str_a, str_b):
-    n, m = len(str_a), len(str_b)
-    if n > m:
-        str_a, str_b, n, m = str_b, str_a, m, n
+"""
+ПРИНЦИП РАБОТЫ
+    Подробное описание алгоритма - https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    *Будем двигаться по рядам матрицы, используя только текущий ряд и предыдущий.
 
-    current_row = [i for i in range(n+1)]
-    for i in range(1, m+1):
-        previous_row, current_row = current_row, [i] + [0] * n
-        for j in range(1, n+1):
-            change = previous_row[j-1] + 1 if str_a[j-1] != str_b[i-1] else previous_row[j-1]
-            current_row[j] = min(previous_row[j]+1, current_row[j-1]+1, change)
-    return current_row[n]
+ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ
+    Доказательство алгоритма - https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+
+ВРЕМЕННАЯ СЛОЖНОСТЬ
+    O(n*m), где n и m — длины строк.
+
+ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ
+    O(n), где n - длина наименьшей строки.
+
+УСПЕШНАЯ ПОСЫЛКА
+    67425496
+"""
+
+
+def levenshtein_distance(first_str, second_str):
+    n, m = len(first_str), len(second_str)
+    if n > m:
+        first_str, second_str, n, m = second_str, first_str, m, n
+
+    current = [i for i in range(n + 1)]
+    for i in range(1, m + 1):
+        previous, current = current, [i] + [0] * n
+        for j in range(1, n + 1):
+            current[j] = min(
+                previous[j] + 1,
+                current[j - 1] + 1,
+                previous[j - 1] + 1 if first_str[j - 1] != second_str[i - 1] else previous[j - 1]
+            )
+    return current[n]
 
 
 print(levenshtein_distance(input(), input()))
